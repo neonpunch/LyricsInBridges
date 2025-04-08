cb8d467e70820123b176bf8d78f79fefa42ced63
clark_0762647 <0762647@my.scccd.edu>
2025-04-08 16:11:17 -0700

    Updates and Debugging

--- GridLyrics.cc
+++ GridLyrics.cc
@@ -45,7 +45,7 @@ vector<string> lyrics_tokenize(const string& lyrics) {
 int main() {
 
 	//create the Bridges object, set credentials
-	Bridges bridges(1001, "neonpunch", "1574857604435");
+	Bridges bridges(1, "neonpunch", "1574857604435");
 
 	bridges.setTitle("Lyrics in Bridges");
 	bridges.setDescription("Artist: AJR, Song: God is Really Real");
@@ -67,13 +67,21 @@ int main() {
 	 */
 	// ANSWER:-KD
 	DataSource ds;
-	Song s = ds.getSong("God is Really Real", "AJR");
+	Song s = ds.getSong("Festival Song", "Jeff Rosenstock");
+	cout << "Song Title: " << s.getSongTitle() << endl;
+	cout << "Artist: " << s.getArtist() << endl;
+
+	s.setLyrics("It feels completely ridiculous That I'm a willing participant Gazing at the purples and pinks In the shadow of a bank-sponsored skyline Unite against the establishment! (While drones transmit the images To a server farm in the valley For a culture that'll eat its own insides) Oh, they wouldn't be your friend if you weren't worth something. They wouldn't be your friend if you weren't worth something They wouldn't be your friend if it wasn't worth it If you didn't have something they could take... ... a long look at the billboards That swallow the air so you can't ignore 'em And glamorize department store crust-punk-chic 'Cause Satan's trending up and it's fashion week But this is not a movement It's just careful entertainment For an easy demographic In our sweatshop denim jackets And we'll wonder what just happened When the world becomes Manhattan Where the banks steal the apartments Just to render them abandoned Oh, they wouldn't be your friend if you weren't worth something They wouldn't be your friend if you weren't worth something They wouldn't be your friend if it wasn't worth it If you didn't have something they could take We're not stupid people, but this financial depression Has got everyone believing all that we can do is nothing 'Cause we organize through avenues they lace with advertisements So the ones we try to rage against are still lining their pockets Oh, they wouldn't be your friend if you weren't worth something They wouldn't be your friend if you weren't worth something They wouldn't be your friend if it wasn't worth it If you didn't have something they could take");
+
+	//cout << "Lyrics Retrieved: " << s.getLyrics() << endl;
 
 	auto words = s.getLyrics();
-
+	
+	//cout << "Lyrics: " << words << endl;
 	vector<string> wordArray = lyrics_tokenize(words);
+	cout << "Total words: " << wordArray.size() << endl;
 
-	ColorGrid grid(wordArray.size(), wordArray.size());
+	ColorGrid grid(wordArray.size(), wordArray.size(), Color(255, 105, 200));
 
 	// input your own RGBA values if you wish
 	Color matchColor (0, 0, 0, 255);
@@ -89,11 +97,11 @@ int main() {
 	 * as that represents each word compared against itself.
 	 */
 
-	for (size_t i = 0; i < wordArray.size(); ++i) {
-		for (size_t j = 0; j < wordArray.size(); ++j) {
-			if (wordArray.at(i) == wordArray.at(j)) {
-				grid.set(i, j, matchColor);
-				grid.set(j, i, matchColor);
+	for (size_t i = 0; i < wordArray.size(); i++) {
+		for (size_t j = i + 1; j < wordArray.size(); j++) {
+			if (wordArray.at(i) != wordArray.at(j)) {
+				grid.set(j, i, Color(0, 0, 0));
+				grid.set(i, j, Color(0, 0, 0));
 			}
 		}
 	}
